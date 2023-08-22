4601d6614de1d6d49e6dd1cd0ca9f6b9c0acf9c0
Evan Crawford <132151681+Sinkcat113@users.noreply.github.com>
2023-08-21 22:31:55 -0500

    OldTube

---
+++ firebase.js
@@ -0,0 +1,12 @@
+const dotenv = require("dotenv")
+dotenv.config()
+
+export const firebaseConfig = {
+    apiKey: process.env.apiKey,
+    authDomain: process.env.authDomain,
+    databaseURL: process.env.databaseURL,
+    projectId: process.env.projectId,
+    storageBucket: process.env.storageBucket,
+    messagingSenderId: process.env.messagingSenderId,
+    appId: process.env.appId
+};
